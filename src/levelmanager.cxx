13dd12ae7aca7885339d931f8268c146b4a6e465
Deanna Hartsook <dmhartsook@gmail.com>
2014-02-04 18:07:50 -0800

    Having issues with image behavior

--- src/levelmanager.cxx
+++ src/levelmanager.cxx
@@ -30,14 +30,16 @@ void LevelManager::step()
 		switch(global_gamestate)
 		{
 			case 0: //Main Menu
-				object_manager->objects_add(new Image(0,0, 400, 100, "media/bg2edit.png"));
+                // TODO: Figure what is wrong with images -> bg only works properly in images? Blobby transparent?
+                object_manager->objects_add(new Image(0,0, 400, 100, "images/bg2edit.bmp"));
+				//object_manager->objects_add(new Image(0,0, 400, 100, "media/bg2edit.png"));
 				object_manager->objects_add(new Button(180,220, 200, 80, ButtonPlay));
 				object_manager->objects_add(new Button(240,290, 200, 80, ButtonInstructions));
 				object_manager->objects_add(new Button(240,355, 400, 100, ButtonQuit));
 				break;
 			case 1: //Level 1
 				object_manager->objects_add(new Image(0,460,640, 20, "images/Ground.bmp"));
-				//~ object_manager->objects_add(new PlayerCharacter(10,10, 32, 32, "images/Block.bmp"));
+				//object_manager->objects_add(new PlayerCharacter(10,10, 32, 32, "images/Block.bmp"));
 				object_manager->objects_add(new PlayerCharacter(10,10, 32, 32, "media/blobbyAttempt.png"));
 				object_manager->objects_add(new Button(580,30, 400, 100, ButtonPause));
 				break;
