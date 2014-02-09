322eebebfa4ff0bfd36bc0b1bc4cc6cd5ccbc2e8
Deanna Hartsook <dmhartsook@gmail.com>
2014-02-08 16:27:48 -0800

    changed name of image from bgIsland1 to island1

--- src/levelmanager.cxx
+++ src/levelmanager.cxx
@@ -37,7 +37,7 @@ void LevelManager::step()
 				object_manager->objects_add(new Button(240,355, 400, 100, ButtonQuit));
 				break;
 			case 1: //Level 1
-                object_manager->objects_add(new Image(0,0, 600, 440, "media/backgrounds/bgisland1.bmp"));
+                object_manager->objects_add(new Image(0,0, 600, 440, "media/backgrounds/island1.bmp"));
 				object_manager->objects_add(new Image(0,460,640, 20, "media/Ground.bmp"));
 				object_manager->objects_add(new PlayerCharacter(10,10, 32, 32, "media/blobbys/blobby.bmp"));
 				object_manager->objects_add(new Button(580,30, 400, 100, ButtonPause));
