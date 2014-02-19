dcad26cf89e5e2c8f05e0f3f957c39a607130ba1
Deanna Hartsook <dmhartsook@gmail.com>
2014-02-19 12:59:25 -0800

    Downloaded edited pngs from Natasha's and saved as bmps. Changed enemy image to torto

--- src/levelmanager.cxx
+++ src/levelmanager.cxx
@@ -43,7 +43,7 @@ void LevelManager::step()
 			case 2: //Level 1
                 object_manager->objects_add(new Image(0,0, 600, 440, "media/backgrounds/island1.bmp"));
 				object_manager->objects_add(new PlayerCharacter(10,10, 32, 32, "media/blobbys/blobby.bmp"));
-				object_manager->objects_add(new Enemy(200,360, 32, 32, "media/blobbys/blobbyTorto.bmp"));
+				object_manager->objects_add(new Enemy(200,360, 32, 32, "media/enemies/torto.bmp"));
 				object_manager->objects_add(new Button(580,30, 400, 100, ButtonPause));
 				break;
 
