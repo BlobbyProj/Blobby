74699a4591559aa4fbeafd82001767137a6e3d9b
Deanna Hartsook <dmhartsook@gmail.com>
2014-02-21 11:45:01 -0800

    Added comments

--- include/levelmanager.h
+++ include/levelmanager.h
@@ -12,14 +12,27 @@ class LevelManager {
 		double level_height;
 		
 	public:
+        // constructor for LevelManager - controls the levels in the game
+        // pre-conditions  LeveltManager does not yet exist
+        // post-conditions LevelManager exists, variables have default values
 		LevelManager() : previous_gamestate(-1), previous_paused(0), level_x(0), level_y(0) {};
 
+        // gets level_x
 		int get_level_x() { return (int)level_x; }
+    
+        // gets level_y
 		int get_level_y() { return (int)level_y; }
+    
+        // gets level_width
 		int get_level_width() { return (int)level_width; }
+    
+        // gets level_height
 		int get_level_height() { return (int)level_height; }
 
+        // sets level_x
 		void set_level_x( double x );
+    
+        //sets level_y
 		void set_level_y( double y );
 
         // called every time there is an event
